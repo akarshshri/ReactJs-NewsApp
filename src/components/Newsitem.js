68db1ef0673846cec73d4edbe9a919d6f5f59578
akarshshri <akarshshrivastava97@gmail.com>
2022-01-07 17:46:18 +0530

    Added Character limit

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let {title,imgDesc, imgUrl, newsUrl} = this.props;
-        
+        let {title,imgDesc, imgUrl, newsUrl, content} = this.props;
+        if (imgDesc === null){
+            imgDesc = content.slice(0,130) + '...';
+        }else if(imgDesc.length >= 170){
+            imgDesc = imgDesc.slice(0,190) + '...';
+        }
         return (
             <>
                 
-                    <div className="card m-auto mt-1" style={{ width: "18rem" }}>
-                        <img src={imgUrl} className="card-img-top" alt="..." />
+                    <div className="card m-auto mt-3 " style={{ width: "18rem" }}>
+                        <img src={imgUrl} className="card-img-top" alt="N/A" style={{height: '12rem'}} />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{imgDesc}</p>
-                            <a href={newsUrl} target="_blank" className="btn btn-primary" rel="noopener noreferrer">Go to News</a>
+                            <a href={newsUrl} target="_blank" className="btn btn-primary" rel="noopener noreferrer">Read More</a>
                         </div>
                     </div>
                 
