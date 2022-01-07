f88a587a2f3bcf24a463101b7c702bd652164d13
akarshshri <akarshshrivastava97@gmail.com>
2022-01-07 18:00:28 +0530

    Updates

---
+++ precache-manifest.28860989fcfc43b378f4dcbdf461a78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77527401b0039d9f6cfa2b31d60ca98a",
+    "url": "/ReactJs-NewsApp/index.html"
+  },
+  {
+    "revision": "3b5fd6b9d1c8277b4e5d",
+    "url": "/ReactJs-NewsApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7d85a135c125d2acef7",
+    "url": "/ReactJs-NewsApp/static/js/2.1785613e.chunk.js"
+  },
+  {
+    "revision": "1ce896dddecdb4d51806",
+    "url": "/ReactJs-NewsApp/static/js/3.c541d426.chunk.js"
+  },
+  {
+    "revision": "3b5fd6b9d1c8277b4e5d",
+    "url": "/ReactJs-NewsApp/static/js/main.e296e238.chunk.js"
+  },
+  {
+    "revision": "a5f02da64445159b6bc6",
+    "url": "/ReactJs-NewsApp/static/js/runtime~main.e0980c9e.js"
+  }
+]);
